b78ad828266620b5fce031b0566274af10d4141f
abfabs <albaeftimi@gmail.com>
2025-09-04 15:49:29 +0000

    factorial

---
+++ debugging/factorial.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import sys
+
+def factorial(n):
+    if n < 0:
+        raise ValueError("n must be >= 0")
+    result = 1
+    while n > 1:
+        result *= n
+        n -= 1
+    return result
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print(f"Usage: {sys.argv[0]} <non-negative-integer>")
+        sys.exit(1)
+    n = int(sys.argv[1])
+    print(factorial(n))
